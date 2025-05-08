b67274b45a256c4daf40cef48d9bc07c25195868
Adam Fellin <adamfellin@gmail.com>
2025-05-08 12:15:23 -0400

    Create ReverseYardline.R

---
+++ ReverseYardline.R
@@ -0,0 +1,6 @@
+pbp <- pbp %>%
+  filter(!is.na(yardline_100)) %>%
+  mutate(
+    yardline = 100 - yardline_100,  # Convert to field position from own endzone
+    yard_group = cut(yardline, breaks = seq(0, 100, by = 10), right = FALSE)
+  )
